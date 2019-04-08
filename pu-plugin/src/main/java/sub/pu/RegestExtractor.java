8ac1b63f31aef13d8288db38580011d5743db0ee
Dennis Neumann <neumann@sub.uni-goettingen.de>
2019-04-08 13:30:08 +0000

    Try out generating queries to find regests

--- pu-plugin/src/main/java/sub/pu/RegestExtractor.java
+++ pu-plugin/src/main/java/sub/pu/RegestExtractor.java
@@ -1,6 +1,11 @@
 package sub.pu;
 
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.solr.client.solrj.SolrClient;
@@ -11,11 +16,18 @@ import org.apache.solr.client.solrj.response.QueryResponse;
 public class RegestExtractor {
 
 	private SolrClient solr;
+	private Set<String> mappings = new HashSet<>();
+	private UmlautWordMapper mapper;
 	
 	public static void main(String[] args) throws Exception {
 		new RegestExtractor().execute(args);
 	}
 
+	public RegestExtractor() {
+		mappings.add("†:j");
+		mappings.add("1:l");
+	}
+	
 	private void execute(String[] args) throws Exception {
 		solr = new HttpSolrClient.Builder("http://localhost:8983/solr").build();
 		
@@ -26,24 +38,75 @@ public class RegestExtractor {
 			processLine(lines[i]);
 		}
 		
-		
 	}
 
 	private void processLine(String line) throws Exception {
 		String[] lineParts = line.split(",");
 		if (lineParts.length < 6)
 			return;
-		
+		System.out.println();
 		System.out.println(line);
+		System.out.println("-------------");
 		String date = lineParts[1];
 		String pope = lineParts[2];
 		String regestNumber = lineParts[4];
 		String page = lineParts[5];
-		System.out.println(regestNumber);
 		
-		SolrQuery solrQuery = new SolrQuery("text:" + regestNumber + " AND page:" + page);
+		mapper = new UmlautWordMapper(mappings);
+
+		List<String> combinations = mapper.createMappings(regestNumber);
+		String regestNumberQuery = generateOrQuery(combinations);
+		String regestNumberQueryWithStars = generateOrQueryWithStars(combinations);
+		
+		String dateFuzzyQuery = generateFuzzyQuery(date);
+		String dateExactQuery = generateDateOrQuery(date);
+		
+		String wholeQuery = "page:" + page + " AND "
+				+ "("
+					+ "text_spaceless:" + regestNumberQueryWithStars + "^10 OR "
+					//+ "text:" + regestNumberQuery + " OR "
+					+ "text:" + dateFuzzyQuery + " OR "
+					+ "text:" + dateExactQuery + " OR "
+					+ "text_spaceless:*" + pope.split(" ")[0] + "*"
+				+ ")";
+		System.out.println(wholeQuery);
+		
+		SolrQuery solrQuery = new SolrQuery(wholeQuery);
 		QueryResponse response = solr.query("pu", solrQuery);
 		String result = (String)response.getResults().get(0).getFieldValue("text");
 		System.out.println(result);
+	}
+	
+	private String generateOrQuery(List<String> combinations) {
+		return createQuery(combinations, "OR", "");
+	}
+	
+	private String generateOrQueryWithStars(List<String> combinations) {
+		return createQuery(combinations, "OR", "*");
+	}
+	
+	private String generateFuzzyQuery(String date) {
+		date = date.replace("(", "").replace(")", "");
+		String[] parts = date.split("[ \\.-]+");
+		return createQuery(new ArrayList<String>(Arrays.asList(parts)), "AND", "~1");
+	}
+	
+	private String generateDateOrQuery(String date) {
+		date = date.replace("(", "").replace(")", "");
+		String[] parts = date.split("[ \\.-]+");
+		return createQuery(new ArrayList<String>(Arrays.asList(parts)), "OR", "");
+	}
+	
+	private String createQuery(List<String> strings, String operator, String suffix) {
+		String query = "(";
+		for (int i = 0; i < strings.size(); i++) {
+			if (i > 0) {
+				query += " " + operator + " ";
+			}
+			query += strings.get(i) + suffix;
+		}
+		query += ")";
+		
+		return query;
 	}
 }
---
+++ pu-plugin/src/main/java/sub/pu/UmlautWordMapper.java
@@ -0,0 +1,78 @@
+package sub.pu;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class UmlautWordMapper {
+
+	private Map<String, String[]> charMappings = new HashMap<>();
+	private List<String> mappedWords = new ArrayList<String>();
+	private String term;
+
+	public UmlautWordMapper(Set<String> mappingsSet) {
+		for (String mapping : mappingsSet) {
+			String[] umlautAndReplacements = mapping.split(":");
+			String umlaut = umlautAndReplacements[0];
+			if (umlaut.startsWith("U+")) {
+				String hex = "0x" + umlaut.substring(2);
+				umlaut = Character.toString((char)(int)Integer.decode(hex));
+			}
+			if (umlautAndReplacements.length >= 2) {
+				String replacements = umlautAndReplacements[1];
+				charMappings.put(umlaut, replacements.split(","));
+			} else {
+				charMappings.put(umlaut, new String[]{""});
+			}
+		}
+	}
+
+	public List<String> createMappings(String currentTerm) {
+		term = currentTerm;
+		mappedWords.add(term);
+
+		int termLength = term.length();
+		for (int i = termLength - 1; i >= 0; i--) {
+			if (i >= 1) {
+				String twoCh = term.substring(i - 1, i + 1);
+				if (isUmlaut(twoCh)) {
+					replaceUmlautAndAddToList(i - 1, i + 1);
+					i--;
+					continue;
+				}
+			}
+			String ch = term.substring(i, i + 1);
+			if (isUmlaut(ch)) {
+				replaceUmlautAndAddToList(i, i + 1);
+			}
+		}
+
+		return mappedWords;
+	}
+
+	private boolean isUmlaut(String ch) {
+		if (charMappings.containsKey(ch)) {
+			return true;
+		}
+		return false;
+	}
+
+	private void replaceUmlautAndAddToList(int from, int to) {
+		List<String> wordsToAdd = new ArrayList<>();
+
+		String umlaut = term.substring(from, to);
+		String[] replacements = charMappings.get(umlaut);
+
+		for (String wordFromList : mappedWords) {
+			String prefix = wordFromList.substring(0, from);
+			String postfix = wordFromList.substring(to);
+			for (String replacement : replacements) {
+				wordsToAdd.add(prefix + replacement + postfix);
+			}
+		}
+		mappedWords.addAll(wordsToAdd);
+	}
+
+}
