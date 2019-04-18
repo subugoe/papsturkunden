6d9a708cb4c2f6f08bae5a190011dbd0bcdeb5ee
Dennis Neumann <neumann@sub.uni-goettingen.de>
2019-04-18 13:45:09 +0000

    Refine the queries

--- pu-plugin/src/main/java/sub/pu/RegestExtractor.java
+++ pu-plugin/src/main/java/sub/pu/RegestExtractor.java
@@ -1,8 +1,11 @@
 package sub.pu;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -18,15 +21,22 @@ public class RegestExtractor {
 	private SolrClient solr;
 	private Set<String> mappings = new HashSet<>();
 	private UmlautWordMapper mapper;
+	private List<String> results = new ArrayList<>();
+	private PrintWriter writer;
 	
 	public static void main(String[] args) throws Exception {
 		new RegestExtractor().execute(args);
 	}
 
-	public RegestExtractor() {
+	public RegestExtractor() throws Exception {
 		mappings.add("†:j");
-		mappings.add("1:l");
-		mappings.add("3:5");
+		mappings.add("0:O");
+		mappings.add("1:l,I");
+		mappings.add("3:\\?");
+		mappings.add("5:•f");
+		mappings.add("6:€");
+		
+		writer = new PrintWriter(new FileWriter("/home/dennis/papsturkunden/search-results.txt"));
 	}
 	
 	private void execute(String[] args) throws Exception {
@@ -39,15 +49,22 @@ public class RegestExtractor {
 			processLine(lines[i]);
 		}
 		
+		Collections.sort(results);
+		for (int i = 0; i < results.size(); i++) {
+			String res = results.get(i);
+			System.out.println(res + "    -- " + i);
+			writer.println(res);
+		}
+		writer.close();
 	}
 
-	private void processLine(String line) throws Exception {
-		String[] lineParts = line.split(",");
+	private void processLine(String csvLine) throws Exception {
+		String[] lineParts = csvLine.split(",");
 		if (lineParts.length < 6)
 			return;
-		System.out.println();
-		System.out.println(line);
-		System.out.println("-------------");
+//		System.out.println();
+//		System.out.println(line);
+//		System.out.println("-------------");
 		String date = lineParts[1];
 		String pope = lineParts[2];
 		String regestNumber = lineParts[4].replace("*", "\\*");
@@ -57,6 +74,7 @@ public class RegestExtractor {
 
 		List<String> combinations = mapper.createMappings(regestNumber);
 		String regestNumberQuery = generateOrQuery(combinations);
+		combinations.add(regestNumber.substring(0, 1));
 		String regestNumberQueryWithStars = generateOrQueryWithStars(combinations);
 		
 		String dateFuzzyQuery = generateFuzzyQuery(date);
@@ -64,21 +82,29 @@ public class RegestExtractor {
 		
 		String wholeQuery = "page:" + page + " AND "
 				+ "("
-					+ "line_spaceless:" + regestNumberQueryWithStars + "^20 OR "
-					+ "line:" + regestNumberQuery + " OR "
-					+ "line:" + dateFuzzyQuery + " OR "
+					+ "line_spaceless:" + regestNumberQueryWithStars + "^35 OR "
+					+ "line:" + regestNumberQuery + "^5 OR "
+					+ "line:" + dateFuzzyQuery + "^3 OR "
 					+ "line:" + dateExactQuery + " OR "
 					+ "next_line:*" + pope.split(" ")[0] + "*"
 				+ ")";
-		System.out.println(wholeQuery);
-		System.out.println("-------------");
+//		System.out.println(wholeQuery);
+//		System.out.println("-------------");
 		
 		SolrQuery solrQuery = new SolrQuery(wholeQuery);
 		QueryResponse response = solr.query("pu", solrQuery);
 		String resultLine = (String)response.getResults().get(0).getFieldValue("line");
 		String nextLine = (String)response.getResults().get(0).getFieldValue("next_line");
-		System.out.println(resultLine);
-		System.out.println(nextLine);
+//		System.out.println(resultLine);
+//		System.out.println(nextLine);
+		
+		String lineNumber = (String)response.getResults().get(0).getFieldValue("id");
+		for (int i = lineNumber.length(); i < 5; i++) {
+			lineNumber = "0" + lineNumber;
+		}
+		results.add(lineNumber + ":   " + resultLine + "     ------    " + wholeQuery);
+//		results.add(lineNumber + ":   " + csvLine);
+//		results.add(lineNumber + ":   " + wholeQuery);
 	}
 	
 	private String generateOrQuery(List<String> combinations) {
